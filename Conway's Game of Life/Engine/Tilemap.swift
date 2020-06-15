bd53c60d46f2c1a36f4a170189617bcf9111e99e
Jon Bash <jonbash@protonmail.com>
2020-06-15 10:51:30 -0700

    add more tilemap stuff

--- Conway's Game of Life/Engine/Tilemap.swift
+++ Conway's Game of Life/Engine/Tilemap.swift
@@ -11,20 +11,19 @@ import Foundation
 
 struct Tilemap {
    private(set) var tiles: [Tile]
-   var width: Int
+   private(set) var width: Int
 
-   init(width: Int, height: Int) {
+   init(width: Int = 1, height: Int = 1) {
       self.tiles = Array(repeating: .dead, count: width * height)
       self.width = width
    }
 }
 
 extension Tilemap {
-   typealias Point = Vector
-   
-   var height: Int { tiles.count / width }
+   var height: Int {
+      get { tiles.count / width }
+   }
    var tileCount: Int { tiles.count }
-
    subscript(_ x: Int, _ y: Int) -> Tile {
       get {
          let index = tileIndex(forX: x, y: y)
@@ -49,9 +48,13 @@ extension Tilemap {
    }
 }
 
-// MARK: - Vectors
+// MARK: - Points
 
 extension Tilemap {
+   func point(fromIndex index: Int) -> Point {
+      Point(x: index % width, y: index / width, tilemap: self)
+   }
+
    func contains(point: Point) -> Bool {
       point.x < width && point.y < height
    }
@@ -60,42 +63,54 @@ extension Tilemap {
       tileIndex(forX: point.x, y: point.y)
    }
 
-   func point(fromIndex index: Int) -> Point {
-      Point(x: index % width, y: index / width)
-   }
-   
    func tile(at point: Point) -> Tile? {
       guard self.contains(point: point) else { return nil }
       return self[point.x, point.y]
    }
-
-   func nextPoint(from point: Point) -> Point? {
-      var index = tileIndex(for: point)
-      index += 1
-      guard self.contains(tileIndex: index) else { return nil }
-      return self.point(fromIndex: index)
-   }
 }
 
-// MARK: - Sequence / Collection
+// MARK: - Sequence
 
 extension Tilemap: Sequence {
    struct Iterator: IteratorProtocol {
       typealias Element = Tile
-      var tilemap: Tilemap
-      var index: Point = .zero
+
+      var index: Point?
+
+      init(tilemap: Tilemap) {
+         self.index = Point(tilemap: tilemap)
+      }
 
       mutating func next() -> Tile? {
          guard
-            let tile = tilemap.tile(at: index),
-            let index = tilemap.nextPoint(from: index)
+            let index = index,
+            let tile = index.tilemap.tile(at: index)
             else { return nil }
-         self.index = index
+         self.index = index.next()
          return tile
       }
    }
 
-   __consuming func makeIterator() -> Iterator {
+   func makeIterator() -> Iterator {
       Iterator(tilemap: self)
+   }
+}
+
+extension Tilemap: Collection {
+   func index(after i: Point) -> Point {
+      var newIndex = i
+      newIndex.tilemap = self
+      return newIndex.next()
+   }
+
+   typealias Element = Tile
+   typealias Index = Point
+
+   var startIndex: Point { point(fromIndex: 0) }
+   var endIndex: Point { point(fromIndex: tileCount - 1) }
+
+   subscript(_ point: Point) -> Tile {
+      get { self[point.x, point.y] }
+      set { self[point.x, point.y] = newValue }
    }
 }
--- Conway's Game of Life/Engine/Vector.swift
+++ Conway's Game of Life/Engine/Vector.swift
@@ -21,3 +21,4 @@ extension Vector {
       Vector(x: 0, y: 0)
    }
 }
+
