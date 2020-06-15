a319b091b0919ab8b76bc761c118b3c403b6b4b2
Jon Bash <jonbash@protonmail.com>
2020-06-15 08:08:39 -0700

    Initial Commit

---
+++ Conway's Game of Life/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Conway's Game of Life
+//
+//  Created by Jon Bash on 2020-06-15.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
